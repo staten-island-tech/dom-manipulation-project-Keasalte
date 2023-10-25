7e60587d56cb944cfde5461122f8d8a0ce9d3dfa
Keasalte <jaychen1347@gmail.com>
2023-10-25 19:46:56 -0400

    Som

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 const DOMSelectors = {
-  form: document.getElementById("form"),
-  name: document.querySelector(".userinput"),
-  image: document.querySelector(".userinput2"),
   button: document.getElementById("button"),
+  text: document.getElementById("text"),
+  box: document.getElementById("box1"),
+  image: document.querySelector(".pictures"),
+  input: document.querySelector(".eee"),
 };
 
-DOMSelectors.form.addEventListener("click", function (e) {  
-    e.preventDefault();
-    e.createthecard();
-    e.inputclear();
-  });
+DOMSelectors.button.addEventListener("click", function(e){
+  e.preventDefault();
+  create();
+  refresh();
+  nuke();
+ }
+);
 
-  function createthecard() {
-     title = DOMSelectors.name.value;
-     image = DOMSelectors.image.value;
-    DOMSelectors.output.insertAdjacentHTML(
-        "afterbegin",
-        `    
-        <section id="result">
-          <div class="output2">
-            <p><img src=>${image}</p>
-             <p>${title}</p>
-             <button class="buttons" id="clear">clear</button>
-         </div>
-        </section>`
-    );
-  }
-  
-  function inputclear(){
-    DOMSelectors.name.value="";
-    DOMSelectors.image.value="";
-  }
+function refresh() {
+  DOMSelectors.input.value = "";
+  DOMSelectors.image.value = "";
+}
+
+
+function create() {
+  let list = document.querySelector("#output");
+  list.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="card-aa">
+    <img src="${DOMSelectors.image.value}" alt="">
+    <p>${DOMSelectors.input.value}</p>
+    <button class="delete">Delete</button>
+    </div>
+    `
+  );
+};
